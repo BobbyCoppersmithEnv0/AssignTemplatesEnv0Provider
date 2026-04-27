7b5776de6745e50cc025cea8586e7fa1d4be3bfb
BobbyCoppersmithEnv0 <robert.coppersmith@env0.com>
2026-04-27 09:42:20 -0700

    Refactor Terraform configuration for template assignments
    Removed project and team assignments while adding template assignments.

--- main.tf
+++ main.tf
@@ -1,38 +1,13 @@
-# Terraform main configuration
-
-# Create the project
-resource "env0_project" "this" {
-  name              = var.project_name
-  description       = var.project_description
-  parent_project_id = var.parent_project_id
-}
-
-# Look up each admin team by name
-data "env0_team" "admin_teams" {
-  for_each = toset(var.admin_teams)
+# Look up each template by name
+data "env0_template" "templates" {
+  for_each = toset(var.template_names)
   name     = each.key
 }
 
-# Look up each viewer team by name
-data "env0_team" "viewer_teams" {
-  for_each = toset(var.viewer_teams)
-  name     = each.key
-}
+# Assign each template to the project
+resource "env0_template_project_assignment" "assignments" {
+  for_each = toset(var.template_names)
 
-# Assign Admin role to specified teams
-resource "env0_team_project_assignment" "admin" {
-  for_each = toset(var.admin_teams)
-
-  team_id    = data.env0_team.admin_teams[each.key].id
-  project_id = env0_project.this.id
-  role       = "Admin"
-}
-
-# Assign Viewer role to specified teams
-resource "env0_team_project_assignment" "viewer" {
-  for_each = toset(var.viewer_teams)
-
-  team_id    = data.env0_team.viewer_teams[each.key].id
-  project_id = env0_project.this.id
-  role       = "Viewer"
+  template_id = data.env0_template.templates[each.key].id
+  project_id  = var.project_id
 }
